24651723774ae19550faac1d654347a50dbba400
youngjin <cherishgad@outlook.com>
2017-10-10 02:55:23 +0900

    finish the all

--- src/pages/park-list/park-list.ts
+++ src/pages/park-list/park-list.ts
@@ -47,6 +47,18 @@ export class ParkListPage {
       this.parks = theResult; 
     }) 
   }
+  customHeaderFn(record, recordIndex, records) { 
+    if ( recordIndex > 0) { 
+      if ( record.name.charAt(0)!==records[recordIndex-1]. 
+              name.charAt(0)) { 
+          return record.name.charAt(0); 
+      } else { 
+          return null; 
+      } 
+    } else { 
+        return record.name.charAt(0); 
+    } 
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ParkListPage');
   }
